a69dd3ed9684d2d6dee0ec51a8b23f8ed8c0d874
madskillskill <mikeitkulof@gmail.com>
2022-04-19 15:42:57 +0400

    making comments and updating readme

--- Project #1/game.py
+++ Project #1/game.py
@@ -1,17 +1,20 @@
 import numpy as np
 
-number = np.random.randint(1,100)
+# This game lets a user guess the number and then hints them if
+# their inaccurate guess is more or less than X.
 
-count = 0
+number = np.random.randint(1,100) # Makes up a number
+
+count = 0 # Sets a counter for tries
 
 while True:
     count += 1
     predict_number = int(input('Guess the number 1 to 100!'))
     if predict_number == number:
-        break
+        break # Win!
     if predict_number <= number:
         print('More')
     if predict_number >= number:
         print('Less')
+        
 print('Congratulations! You\'ve done it in',count,'tries!')
-
